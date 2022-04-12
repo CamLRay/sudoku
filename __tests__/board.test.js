1186430463f6338c6ada2a61628298aaeaa2a0e6
William Rodriguez <william@rodriguez1.com>
2022-04-12 14:50:41 -0700

    Add row check > > > Co-Authored-by: Gabe Nielsen <gabenielsen7@gmail.com> >

--- __tests__/board.test.js
+++ __tests__/board.test.js
@@ -6,10 +6,25 @@ describe("board", () => {
     boardArray.push([0, 0, 0, 0, 0, 0, 0, 0, 0]);
   }
 
-  test("It should create an object with a property that contains the given 9x9 2D array", () => {
-    let newBoardArray = boardArray.concat([]);
-    const board = new Board(newBoardArray);
+  let newBoardArray = boardArray.concat([]);
+  const board = new Board(newBoardArray);
 
+  test("It should create an object with a property that contains the given 9x9 2D array", () => {
     expect(board.boardArray).toEqual(newBoardArray);
+  });
+
+  test("It should add the given number to the given position in boardArray", () => {
+    board.addNumber(5, 0, 0);
+    expect(board.boardArray[0][0]).toEqual(5);
+  });
+
+  test("It should return true if the given row is a valid row", () => {
+    expect(board.checkRow(0)).toEqual(true);
+  });
+
+  test("It should return true if the given row is a valid row", () => {
+    board.addNumber(5, 0, 0);
+    board.addNumber(5, 1, 0);
+    expect(board.checkRow(0)).toEqual(false);
   });
 });
--- src/board.js
+++ src/board.js
@@ -1,3 +1,22 @@
 export function Board(boardArray) {
   this.boardArray = boardArray;
 }
+
+Board.prototype.addNumber = function(number, x, y) { 
+  this.boardArray[y][x] = number;
+};
+
+Board.prototype.checkRow = function(rowNumber) {
+  const row = this.boardArray[rowNumber];
+  let prevNumbers = [];
+
+  for (let i = 0; i < row.length; i++) {
+    if (prevNumbers.includes(row[i])) {
+      return false;
+    } else if (row[i] !== 0) {
+      prevNumbers.push(row[i]);
+    }
+  }
+
+  return true;
+}
